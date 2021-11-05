f248a6fad64d89e676ab29d92f816a05e6a1a3ba
tre3ubec <ekoroteev27@yandex.ru>
2021-11-05 15:03:36 +0300

    clear input after submit & trim() for input

--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
-
-// class Trash extends React.Component {
-//  render() {
-//   return(
-//    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-//     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-//     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-//    </svg>
-//   )
-//  }
-// }
+import { Trash } from 'react-bootstrap-icons';
 
 class NewItem extends React.Component {
  render() {
@@ -36,6 +26,7 @@ class NewItem extends React.Component {
      type="text"
      placeholder={placeholderNewItem}
      onChange={this.props.change}
+     value={this.props.value}
     />
     <button className="btn btn-outline-secondary" type="submit" >Add</button>
    </form>
@@ -68,11 +59,12 @@ class TodoItem extends React.Component {
  render() {
   return(
    <div className="todoItem input-group mb-3">
-
     <div className={this.state.backgroundForm.join(' ')}>
      <input className="form-check-input mt-0" type="checkbox" onChange={this.done} defaultChecked={this.state.checked}/>
     </div>
+
     <input type="text" className={this.state.task.join(' ')} defaultValue={this.props.value} />
+    {/*<Trash className="trash" color="gray" size={25} />*/}
    </div>
  )
  }
@@ -97,7 +89,7 @@ class TodoList extends React.Component {
  itemSubmit = (event) => {
   event.preventDefault()
   this.setState({
-   items: [...this.state.items, {id: this.state.items.length + 1, value: this.state.value}],
+   items: [...this.state.items, {id: this.state.items.length + 1, value: this.state.value.trim('')}],
    value: ''
   })
  }
@@ -105,7 +97,7 @@ class TodoList extends React.Component {
  render() {
   return(
    <div className="todoList">
-    <NewItem submit={this.itemSubmit} change={this.itemChange}/>
+    <NewItem submit={this.itemSubmit} change={this.itemChange} value={this.state.value}/>
     { this.state.items.map(el => <TodoItem key={el.id} value={el.value} />) }
    </div>
   )
