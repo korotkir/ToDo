67c42f7099dc17b230d2785f50a7938c1833cfcb
tre3ubec <ekoroteev27@yandex.ru>
2021-11-11 22:48:49 +0300

    add icon & font

--- src/todo/TodoItems.js
+++ src/todo/TodoItems.js
@@ -1,15 +1,17 @@
 import React from "react";
-import Item from "./Item";
+import Item from "./items/Item";
+import Animation from "./items/Animation";
 
 class TodoItems extends React.Component {
  render() {
   return (
+   this.props.items.length ?
    [...this.props.items].map((el, i) =>
      <Item key={el.id}
            value={el.value}
            onClick={(e) => this.props.onClick(this.props.items[i], e)}
            items={this.props.items}
-     />)
+     />) : <div className="animation__start"><Animation /></div>
   )
  }
 }
---
+++ src/todo/items/Animation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Player } from '@lottiefiles/react-lottie-player';
+import success from "../icon/success.json";
+
+class Animation extends React.Component {
+ constructor(props) {
+  super(props);
+  this.player = React.createRef(); // initialize your ref
+ }
+ render() {
+  return (
+   <Player
+    className="startAnimation"
+    ref={this.player} // set the ref to your class instance
+    autoplay={true}
+    loop={true}
+    controls={true}
+    src={success}
+    style={{ height: '300px', width: '300px' }}
+   ></Player>
+  );
+ }
+}
+
+export default Animation;
