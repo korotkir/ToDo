21bc322d4fb73050533d23b4e0dc13eb5afb1a76
tre3ubec <ekoroteev27@yandex.ru>
2022-02-03 19:48:41 +0300

    fixed bug with TodoStatus

--- src/todo/TodoStatus.js
+++ src/todo/TodoStatus.js
@@ -3,10 +3,11 @@ import React from 'react'
 const TodoStatus = props => {
   return (
         <div className="TodoStatus row justify-content-center">
-          {
-            props.done
-              ? <h2 className="status col-auto">{props.done}/{props.values}</h2>
-              : <h2 className="status col-auto">{props.values}</h2>
+          {props.values
+            ? props.done
+                ? <h2 className="status col-auto">{props.done}/{props.values}</h2>
+                : <h2 className="status col-auto">{props.values}</h2>
+            : null
           }
         </div>
   )
