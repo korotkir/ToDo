b5fcce6749e39942fd83a216b58c1fa0382ce651
tre3ubec <ekoroteev27@yandex.ru>
2022-04-19 17:46:29 +0300

    removed logs

--- build/precache-manifest.014600cf2955db7c8905bd8483f98efc.js
+++ build/precache-manifest.014600cf2955db7c8905bd8483f98efc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a976226e9549f0782183f3729eb9a19f",
+    "revision": "16a55a416b5054fe43a022dec015e690",
     "url": "/ToDo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ToDo/static/css/2.171a7357.chunk.css"
   },
   {
-    "revision": "1eda7f4a9f2139dd95ed",
+    "revision": "c532c006949c9f71067f",
     "url": "/ToDo/static/css/main.12513200.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ToDo/static/js/2.3e3a77a3.chunk.js"
   },
   {
-    "revision": "1eda7f4a9f2139dd95ed",
-    "url": "/ToDo/static/js/main.51714ade.chunk.js"
+    "revision": "c532c006949c9f71067f",
+    "url": "/ToDo/static/js/main.4351fc95.chunk.js"
   },
   {
     "revision": "e73a18360c7d79d3975a",
