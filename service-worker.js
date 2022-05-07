ccaa1e56327550398ac85a348a42a17523829575
tre3ubec <ekoroteev27@yandex.ru>
2022-05-07 11:50:08 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToDo/precache-manifest.014600cf2955db7c8905bd8483f98efc.js"
+  "/ToDo/precache-manifest.c0fb62ff77ac0beba5ae59a250fdd207.js"
 );
 
 self.addEventListener('message', (event) => {
